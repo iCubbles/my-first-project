c96765962ff3fa72f4abb294617aae839bb06835
edwingamboa <edwingamboa355@gmail.com>
2018-10-01 08:57:35 -0500

    Add demo webpackage

---
+++ webpackages/my-first-webpackage/README.md
@@ -0,0 +1,40 @@
+## my-first-webpackage
+Webpackage to demonstrate the use of Cubbles components
+### Webpackage Artifacts
+| Name | Type | Description | Links |
+|---|---|---|---|
+| **docs** | Application | Generated webpackage documentation. | [docs](https://cubbles.world/sandbox/my-first-webpackage@0.1.0-SNAPSHOT/docs/index.html) |
+| **currency-converter** | Elementary Component | Elementary that uses an api to Convert currencies | [demo](https://cubbles.world/sandbox/my-first-webpackage@0.1.0-SNAPSHOT/currency-converter/demo/index.html) [docs](https://cubbles.world/sandbox/my-first-webpackage@0.1.0-SNAPSHOT/currency-converter/docs/index.html) |
+| **currency-viewer** | Compound Component | Component to display currency differences in a bar-chart | [demo](https://cubbles.world/sandbox/my-first-webpackage@0.1.0-SNAPSHOT/currency-viewer/demo/index.html) [docs](https://cubbles.world/sandbox/my-first-webpackage@0.1.0-SNAPSHOT/currency-viewer/docs/index.html) |
+| **currency-viewer-init** | Compound Component | Component to display currency differences in a bar-chart, with initialisation | [demo](https://cubbles.world/sandbox/my-first-webpackage@0.1.0-SNAPSHOT/currency-viewer-init/demo/index.html) [docs](https://cubbles.world/sandbox/my-first-webpackage@0.1.0-SNAPSHOT/currency-viewer-init/docs/index.html) |
+### Use of components
+The html file should contain the desire component using its tag, e.g. the `<currency-converter>`, as follows:
+```html
+<currency-converter cubx-webpackage-id="my-first-webpackage@0.1.0-SNAPSHOT"></currency-converter>
+```
+Note that the `webpackageId` should be provided as attribute, which in this case is: `my-first-webpackage@0.1.0-SNAPSHOT`.
+
+Additionally, this component can be initialized using the `<cubx-core-slot-init>` tag (available from _cubx.core.rte@1.9.0_).
+For example, lets initialize the `base` slot to get the basic package of ckeditor:
+
+```html
+<currency-converter cubx-webpackage-id="my-first-webpackage@0.1.0-SNAPSHOT">
+    <!--Initilization-->
+    <cubx-core-init style="display:none">
+        <cubx-core-slot-init slot="base">EUR</cubx-core-slot-init>
+    </cubx-core-init>
+</currency-converter>
+```
+
+Or it can be initialized and later manipulated from Javascript as follows:
+
+```javascript
+var component= document.querySelector('currency-converter');
+// Wait until CIF is ready
+document.addEventListener('cifReady', function() {
+  // Manipulate slots
+  component.setBase(EUR);
+});
+```
+
+[Want to get to know the Cubbles Platform?](https://cubbles.github.io)
---
+++ webpackages/my-first-webpackage/currency-converter/currency-converter.js
@@ -0,0 +1,126 @@
+(function () {
+  'use strict';
+
+  CubxComponent({
+    is: 'currency-converter',
+
+    /**
+     * Manipulate an element’s local DOM when the element is created.
+     */
+    created: function () {
+    },
+
+    /**
+     * Manipulate an element’s local DOM when the element is created and initialized.
+     */
+    ready: function () {
+    },
+
+    /**
+     * Manipulate an element’s local DOM when the element is attached to the document.
+     */
+    connected: function () {
+    },
+
+    /**
+     * Manipulate an element’s local DOM when the element is dettached to the document.
+     */
+    disconnected: function () {
+    },
+
+    /**
+     * Manipulate an element’s local DOM when the cubbles framework is initialized and ready to work.
+     */
+    contextReady: function () {
+      this.$.base.setAttribute('value', this.getBase());
+      this.$.foreign.setAttribute('value', this.getForeignCurrency());
+      this.$.date.setAttribute('value', this.getDate());
+      this.sendQuery();
+
+      this.$.convertBtn.addEventListener('click', function() {
+        this.getFormValuesAndSendQuery();
+      }.bind(this));
+      this.$.convertBtn.removeAttribute('disabled');
+    },
+
+    /**
+      * Observe the 'base' slot to update the view of this component and then
+      * send the request to the fixer-io api
+      * @param {string} newValue - new value of the slot
+      */
+    modelBaseChanged: function (newValue) {
+      // update the view
+      this.$.base.setAttribute('value', newValue);
+    },
+    /**
+     * Observe the 'date' slot to update the view of this component and then
+     * send the request to the fixer-io api
+     * @param {string} newValue - new value of the slot ('yyyy-mm-dd' format)
+     */
+    modelDateChanged: function (newValue) {
+      // update the view
+      this.$.date.setAttribute('value', newValue);
+    },
+    /**
+     * Observe the slot 'foreignCurrency' to update the view of this component
+     * and then send the request to the fixer-io api.
+     * @param {string} newValue - new value of the slot
+     */
+    modelForeignCurrencyChanged: function (newValue) {
+      // update the view
+      this.$.foreign.setAttribute('value', newValue);
+    },
+    /**
+     * Observe the slot 'conversion' to update the view of this component
+     * @param {string} newValue - new value of the slot
+     */
+    modelConversionChanged: function (newValue) {
+      // update the view
+      this.$.result.innerHTML = '1 ' + this.getBase() + ' -> ' +
+        '<b>' + newValue + '</b> ' + this.getForeignCurrency() + ' on ' + this.getDate();
+    },
+    /**
+     * Update the Component-Model and then send the request
+     * to the fixer-io api.
+     * @param event
+     */
+    getFormValuesAndSendQuery: function () {
+      // Update the Cubbles component model slots the setters
+      this.setBase(this.$.base.value);
+      this.setDate(this.$.date.value);
+      this.setForeignCurrency(this.$.foreign.value);
+
+      this.sendQuery();
+    },
+
+    /**
+     * Send the request a to the fixer-io api.
+     */
+    sendQuery: function () {
+      // Makes sure all slots are defined
+      if (this.getBase() && this.getForeignCurrency()) {
+        var conversionKey = this.getBase() + '_' + this.getForeignCurrency();
+        var queryDate = this.getDate();
+        var baseUrl = 'https://free.currencyconverterapi.com/api/v6/convert';
+        var queryUrl = baseUrl + '?q=' + conversionKey +
+          '&date=' + queryDate + '&compact=y';
+        
+        var self = this;
+
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+          if (this.readyState == 4 && this.status == 200) {
+            var data = JSON.parse(xhttp.responseText);
+            console.log(data);
+            // Update the Cubbles component model slots using the setters
+            var converted = data[conversionKey]['val'][queryDate];
+            self.setConversion(converted);
+            self.setConversionArray([[self.getBase(), 1],[self.getForeignCurrency(), converted]]);
+          }
+        };
+        xhttp.open("GET", queryUrl, true);
+        xhttp.send();
+      }
+    }
+  });
+}());
