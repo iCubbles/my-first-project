d53c34e843706b32d553028d7a6c7b6f3b2cd75a
edwingamboa <edwingamboa355@gmail.com>
2018-10-13 21:11:37 -0500

    cubbles/gitbook#5: Fix errors

--- webpackages/my-first-webpackage/currency-converter/test/currency-converter-test.js
+++ webpackages/my-first-webpackage/currency-converter/test/currency-converter-test.js
@@ -5,7 +5,7 @@ describe('<currency-converter>', function () {
   this.timeout(70000);
   var currencyConverter;
   var makeRequestStub;
-  var vals = {
+  var expectedVals = {
     init: {
       base: 'EUR',
       foreignCurrency: 'USD',
@@ -18,23 +18,24 @@ describe('<currency-converter>', function () {
       date: '2018-07-07',
       conversion: 2
     }
-  }
+  };
   before(function (done) {
     // Select component
     currencyConverter = document.querySelector('currency-converter');
     var rootNode = document.querySelector('[cubx-core-crc]');
-    // Wait until the component is ready, but has not been initialized
+
+    // Wait until the component is ready
     rootNode.addEventListener('cifInitReady', function () {
-      // Mock sendQuery since it makes Ajax requests
+      // Wrap sendQuery since it makes Ajax requests
       makeRequestStub = sinon.stub(currencyConverter, 'makeRequest', function (url, callback) {
         var data = {};
         var values;
         switch (currencyConverter.getBase()) {
-          case vals.init.base:
-            values = vals.init;
+          case expectedVals.init.base:
+            values = expectedVals.init;
             break;
-          case vals.set.base:
-            values = vals.set;
+          case expectedVals.set.base:
+            values = expectedVals.set;
             break;
           default:
             converted = null;
@@ -56,37 +57,39 @@ describe('<currency-converter>', function () {
   });
 
   describe('should have initial values set in manifest', function () {
-    it('should set the value of "foreignCurrency" slot = "' + vals.init.foreignCurrency + '"', function () {
-      expect(currencyConverter.getForeignCurrency()).to.be.equal(vals.init.foreignCurrency);
+    it('should set the value of "foreignCurrency" slot = "' + expectedVals.init.foreignCurrency + '"', function () {
+      expect(currencyConverter.getForeignCurrency()).to.be.equal(expectedVals.init.foreignCurrency);
     });
-    it('should have the initial value of "date" slot = "' + vals.init.date + '"', function () {
-      expect(currencyConverter.getDate()).to.be.equal(vals.init.date);
+    it('should have the initial value of "date" slot = "' + expectedVals.init.date + '"', function () {
+      expect(currencyConverter.getDate()).to.be.equal(expectedVals.init.date);
     });
-    it('should have the initial value of "base" slot = "' + vals.init.base + '"', function () {
-      expect(currencyConverter.getBase()).to.be.equal(vals.init.base);
+    it('should have the initial value of "base" slot = "' + expectedVals.init.base + '"', function () {
+      expect(currencyConverter.getBase()).to.be.equal(expectedVals.init.base);
     });
     it('should make the request with the initial values and set ', function () {
       expect(makeRequestStub.calledOnce).to.be.equal(true);
-      expect(currencyConverter.getConversion()).to.be.equal(vals.init.conversion);
+      expect(currencyConverter.getConversion()).to.be.equal(expectedVals.init.conversion);
     });
   });
 
   describe('should set slots values correctly', function () {
-    it('should have the initial value of "foreignCurrency" slot = "' + vals.set.foreignCurrency + '"', function () {
-      currencyConverter.setForeignCurrency(vals.set.foreignCurrency)
-      expect(currencyConverter.getForeignCurrency()).to.be.equal(vals.set.foreignCurrency);
+    before(function () {
+      currencyConverter.setForeignCurrency(expectedVals.set.foreignCurrency);
+      currencyConverter.setDate(expectedVals.set.date);
+      currencyConverter.setBase(expectedVals.set.base);
+      currencyConverter.sendQuery();
     });
-    it('should have the initial value of "date" slot = "' + vals.set.date + '"', function () {
-      currencyConverter.setDate(vals.set.date)
-      expect(currencyConverter.getDate()).to.be.equal(vals.set.date);
+    it('should have the initial value of "foreignCurrency" slot = "' + expectedVals.set.foreignCurrency + '"', function () {
+      expect(currencyConverter.getForeignCurrency()).to.be.equal(expectedVals.set.foreignCurrency);
     });
-    it('should have the initial value of "base" slot = "' + vals.set.base + '"', function () {
-      currencyConverter.setBase(vals.set.base)
-      expect(currencyConverter.getBase()).to.be.equal(vals.set.base);
+    it('should have the initial value of "date" slot = "' + expectedVals.set.date + '"', function () {
+      expect(currencyConverter.getDate()).to.be.equal(expectedVals.set.date);
+    });
+    it('should have the initial value of "base" slot = "' + expectedVals.set.base + '"', function () {
+      expect(currencyConverter.getBase()).to.be.equal(expectedVals.set.base);
     });
     it('should make the request with the initial values and set ', function () {
-      currencyConverter.sendQuery();
-      expect(currencyConverter.getConversion()).to.be.equal(vals.set.conversion);
+      expect(currencyConverter.getConversion()).to.be.equal(expectedVals.set.conversion);
     });
   });
 });
