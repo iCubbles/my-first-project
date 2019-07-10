8ec90328b88182fd8097f868bcafc26235ad28d2
edwingamboa <edwingamboa355@gmail.com>
2019-07-10 07:03:06 -0500

    cubbles/cubbles#42 Use exhangeratesapi and undefined reponses handling

--- webpackages/my-first-webpackage/currency-converter/currency-converter.js
+++ webpackages/my-first-webpackage/currency-converter/currency-converter.js
@@ -27,16 +27,25 @@
     /**
      * Manipulate an element’s local DOM when the cubbles framework is initialized and ready to work.
      */
-    contextReady: function () {
-      this.$.base.setAttribute('value', this.getBase());
-      this.$.foreign.setAttribute('value', this.getForeignCurrency());
-      this.$.date.setAttribute('value', this.getDate());
+    contextReady: function() {
+      this.$.base.setAttribute("value", this.getBase());
+      this.$.foreign.setAttribute("value", this.getForeignCurrency());
+      let today = new Date();
+      this.setDate(today.toISOString().substr(0, 10));
+      this.$.date.setAttribute("value", this.getDate());
+      this.$.date.setAttribute("max", this.getDate());
+      today.setMonth(today.getMonth() - 12);
+      this.$.date.setAttribute("min", today.toISOString().substr(0, 10));
+
       this.sendQuery();
 
-      this.$.convertBtn.addEventListener('click', function () {
-        this.getFormValuesAndSendQuery();
-      }.bind(this));
-      this.$.convertBtn.removeAttribute('disabled');
+      this.$.convertBtn.addEventListener(
+        "click",
+        function() {
+          this.getFormValuesAndSendQuery();
+        }.bind(this)
+      );
+      this.$.convertBtn.removeAttribute("disabled");
     },
 
     /**
@@ -44,43 +53,48 @@
      * send the request to the fixer-io api
      * @param {string} newValue - new value of the slot
      */
-    modelBaseChanged: function (newValue) {
+    modelBaseChanged: function(newValue) {
       // update the view
-      this.$.base.setAttribute('value', newValue);
+      this.$.base.setAttribute("value", newValue);
     },
     /**
      * Observe the 'date' slot to update the view of this component and then
      * send the request to the fixer-io api
      * @param {string} newValue - new value of the slot ('yyyy-mm-dd' format)
      */
-    modelDateChanged: function (newValue) {
+    modelDateChanged: function(newValue) {
       // update the view
-      this.$.date.setAttribute('value', newValue);
+      this.$.date.setAttribute("value", newValue);
     },
     /**
      * Observe the slot 'foreignCurrency' to update the view of this component
      * and then send the request to the fixer-io api.
      * @param {string} newValue - new value of the slot
      */
-    modelForeignCurrencyChanged: function (newValue) {
+    modelForeignCurrencyChanged: function(newValue) {
       // update the view
-      this.$.foreign.setAttribute('value', newValue);
+      this.$.foreign.setAttribute("value", newValue);
     },
     /**
      * Observe the slot 'conversion' to update the view of this component
      * @param {string} newValue - new value of the slot
      */
-    modelConversionChanged: function (newValue) {
-      // update the view
-      this.$.result.innerHTML = '1 ' + this.getBase() + ' -> ' +
-        '<b>' + newValue + '</b> ' + this.getForeignCurrency() + ' on ' + this.getDate();
+    modelConversionChanged: function(newValue) {
+      let resultText;
+      if (newValue) {
+        // update the view
+        resultText = `1 ${this.getBase()} -> <b>${newValue}</b> ${this.getForeignCurrency()} on ${this.getDate()}`
+      } else {
+        resultText = 'Invalid inputs.'
+      }
+      this.$.result.innerHTML = resultText;
     },
     /**
      * Update the Component-Model and then send the request
      * to the fixer-io api.
      * @param event
      */
-    getFormValuesAndSendQuery: function () {
+    getFormValuesAndSendQuery: function() {
       // Update the Cubbles component model slots the setters
       this.setBase(this.$.base.value);
       this.setDate(this.$.date.value);
@@ -92,42 +106,45 @@
     /**
      * Send the request a to the fixer-io api.
      */
-    sendQuery: function () {
+    sendQuery: function() {
       // Makes sure all slots are defined
       if (this.getBase() && this.getForeignCurrency()) {
-        var conversionKey = this.getBase() + '_' + this.getForeignCurrency();
-        var queryDate = this.getDate();
-        var baseUrl = 'https://free.currencyconverterapi.com/api/v6/convert';
-        var queryUrl = baseUrl + '?q=' + conversionKey +
-          '&date=' + queryDate + '&compact=y';
+        var baseUrl = "https://api.exchangeratesapi.io";
+        var queryUrl = `${baseUrl}/${this.getDate()}?base=${this.getBase()}&symbols=${this.getForeignCurrency()}`;
         var self = this;
-        
-        function processRespond (data) {
-          // Update the Cubbles component model slots using the setters
-          var converted = data[conversionKey]['val'][queryDate];
+
+        function processRespond(data) {
+          var converted;
+          if (data) {
+            // Update the Cubbles component model slots using the setters
+            converted = data.rates[self.getForeignCurrency()];
+          }
           self.setConversion(converted);
-          self.setConversionArray([
-            [self.getBase(), 1],
-            [self.getForeignCurrency(), converted]
-          ]);
+          if (converted) {
+            self.setConversionArray([
+              [self.getBase(), 1],
+              [self.getForeignCurrency(), converted]
+            ]);
+          } else {
+            self.setConversionArray([]);
+            }
         }
 
-        this.makeRequest(queryUrl, processRespond)
-
+        this.makeRequest(queryUrl, processRespond);
       }
     },
 
     /**
      * Make the ajax request
      */
-    makeRequest: function (queryUrl, processRespond) {
-      var self = this;
-
+    makeRequest: function(queryUrl, processRespond) {
       var xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function () {
+      xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
           var data = JSON.parse(xhttp.responseText);
           processRespond(data);
+        } else {
+          processRespond();
         }
       };
       xhttp.open("GET", queryUrl, true);
